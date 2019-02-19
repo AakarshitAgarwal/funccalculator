7254ff175615e8b1db7025a0e006d8867e0a0360
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2019-02-20 02:21:44 +0530

    commit1

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void add(int a,int b)
+{
+	int sum;
+	sum=a+b;
+	cout<<sum;
+}
+
+
+int main()
+{
+	int a=10,b=5;
+	cout<<"sum of two no's is "<<endl;
+	add(a,b);
+	return(0);
+}
